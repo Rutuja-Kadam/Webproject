c9d2dc5599446409eefc199d134bcf46c5394c9c
Rutuja <kadamrutuja85@gmail.com>
2026-08-18 23:11:50 -0400

    Initial commit of Login and SmartERP projects

---
+++ Login/Login/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid) {
+                if (model.UserName == "Admin" & model.Password == "1234")
+                {
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    ViewBag.Error = "Invalid USerName and Passwords";
+                }
+
+                 }
+            return View(model);
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Login/Login/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Login.Models
+    
+{
+    public class LoginModel
+    {
+        [Required]
+
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
